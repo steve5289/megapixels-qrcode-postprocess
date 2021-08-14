7bd9c914888ca49d82eecac2e6b18ecdef39b682
steve <steve5289@users.noreply.github.com>
2021-08-14 13:51:37 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Megapixels qrcode postprocess script
+The purpose of this script is to provide a simple way to view qr codes from megapixels. Intended for use with a pinephone or librem 5.
+
+# How it works
+1. Install the postprocess script (see below)
+2. Open up megapixels and move your phone to take a picture of a qr code.
+3. If a QR code is detected a blue box will appear around the qr code (this is via megapixel's own processing not this script)
+4. Take a picture of the qr code with that blue box around it.
+5. Wait for the picture to be processed(You'll see a spinning thing in the bottom left). This may take a couple of seconds
+6. A dialog box should pop up saying a qr code was found in the provided image. Depending on the qr code, it will ask if you want to see the contents or perform an action based on the contents. 
+
+# Installation Instructions
+1. First you will need to install the following via apt-get:
+```
+sudo apt-get install megapixels zenity zbar-tools
+```
+2. Then you need to make a directory for the postprocess script to live
+```
+mkdir -p ~purism/.config/megapixels
+```
+3. Download the postprocess script from https://raw.githubusercontent.com/steve5289/megapixels_qr_code_postprocess/main/postprocess.sh, and place it in the ~purism/.config/megapixels/ keeping the name postprocess.sh
+```
+cd ~purism/.config/megapixels/
+wget https://raw.githubusercontent.com/steve5289/megapixels_qr_code_postprocess/main/postprocess.sh
+```
+4. Set the permissions on the script to 755 (basically make it executable)
+```
+chmod 755 postprocess.sh
+```
---
+++ postprocess.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+## Script Description
+# This script's purpose is to act as a postprocessing script as part of megapixe's image processing by detecting, and performing actions based on them (with user input).
+
+
+CLEAN_DNG_FILE=1
+STOCK_POSTPROCESS_PATH="/usr/share/megapixels/postprocess.sh"
+
+## 
+# Execution Starts here
+function main {
+    local BURST_DIR="$1"
+    local IMAGE_NAME="$2"
+    local DNG_FILE="$2.dng"
+    local IMAGE_FILE
+
+    run_stock_postprocess "$@"
+
+    get_image_file "$IMAGE_NAME"
+    if [[ -n "$OUTPUT" ]]; then
+        IMAGE_FILE="$OUTPUT"
+        if [[ "$CLEAN_DNG_FILE" -eq 1 ]]; then
+            rm -f "$DNG_FILE"
+        fi
+    else
+        die "Error! Could not find image file with name '$IMAGE_NAME'!"
+    fi
+
+    qr_code "$IMAGE_FILE"
+}
+
+##
+# As we are only given a image name (no extention like .jpg) we need to figure out what that is.
+function get_image_file {
+    local IMAGE_NAME="$1"
+    local file
+
+    for file in "$IMAGE_NAME."{jpg,tiff,png}; do
+        if [[ -e "$file" ]]; then
+            OUTPUT="$file"
+            return
+        fi
+    done
+}
+
+##
+# Run the normal mexgapixels postprocess script so that we don't need to maintain a version of that.
+function run_stock_postprocess {
+    $STOCK_POSTPROCESS_PATH "$@" || exit $?
+}
+
+##
+# Process the image for a qr code
+function qr_code {
+    local IMAGE_FILE="$1"
+    local QR_CODE
+    
+    qr_code_check_deps || die "Error dependancies not met for qr code scanning"
+
+    QR_CODE=$(zbarimg -q "$IMAGE_FILE" | sed 's/^QR-Code://')
+
+    # If no qr code found, then just exit
+    if [[ -z "$QR_CODE" ]]; then
+        echo "QR Code not found... exiting"
+        exit 0
+    fi
+
+    qr_code_launch "$QR_CODE"
+    rm -f "$IMAGE_FILE" 
+    
+}
+
+##
+# Ensure the needed dependancies are installed to run this.
+function qr_code_check_deps {
+    command -v zbarimg > /dev/null 2>&1 || return 1
+    command -v zenity > /dev/null 2>&1 || return 1
+    return 0
+}
+
+##
+# Depending on the type of qr code perform different actions. Always ask the user before doing anything.
+function qr_code_launch {
+    local QR_CODE="$1"
+
+    # EMAIL
+    if [[ "$QR_CODE" = mailto:* ]]; then
+        local EMAIL=$(echo "" | sed 's/^mailto://')
+        prompt_yn "This image contains a qr code to send an email to $EMAIL. Would you like to send an email to them?" || exit 0
+
+        xdg-open "$QR_CODE"
+
+    # CONTACT INFO
+    elif [[ "$QR_CODE" = MECARD:* ]] ; then
+        prompt_yn "This image contains a qr code for contact information for someone. See text information?" || exit 0
+
+        new_tempfile "contact" ".txt"
+        TMPFILE="$OUTPUT"
+        echo "$QR_CODE" | sed 's/^MECARD://' | sed 's/;/\n/g' > "$TMPFILE"
+        xdg-open "$TMPFILE"
+
+    # GEO LOCATION
+    elif [[ "$QR_CODE" = geo:* ]] ; then
+        prompt_yn "This image contains a qr code for geo location. Open in map?" || exit 0
+        xdg-open "$QR_CODE"
+
+    # TELEPHONE NUMBER
+    elif [[ "$QR_CODE" = tel:* ]] ; then
+        local TEL=$(echo "$QR_CODE" | sed 's/^tel://')
+        prompt_yn "This image contains a qr code for the telephone numer '$TEL'. Would you like to call it?" || exit 0
+        xdg-open "$QR_CODE"
+
+    # SMS NUMBER
+    elif [[ "$QR_CODE" = smsto:* ]] ; then
+        IFS=$':'
+        local RAW=( $(echo "$QR_CODE" | sed 's/^smsto://') )
+        IFS=$' \t\n'
+        local TEL="${RAW[0]}"
+        local MESS="${RAW[1]}"
+        prompt_yn "This image contains a qr code that wants you to send a sms message to a number. Would you like to see it?" || exit 0
+        new_tempfile "smsinfo" ".txt"
+        TMPFILE="$OUTPUT"
+        echo -e "Phone Number: $TEL\nMessage to send to it: '$MESS'" > "$TMPFILE"
+        xdg-open "$TMPFILE"
+        
+
+    # URL
+    elif [[ "$QR_CODE" = http*://* ]] ; then
+        prompt_yn "This image contains the URL '$QR_CODE'. Would you like to open this in the web broswer?" || exit 0
+        xdg-open "$QR_CODE"
+
+    # Anything else
+    else
+        prompt_yn "This image contains unknown information. Would you like to see the raw text?" || exit 0
+
+        new_tempfile "qrcode" ".txt"
+        TMPFILE="$OUTPUT"
+        echo "$QR_CODE" > "$TMPFILE"
+        xdg-open "$TMPFILE"
+    fi
+
+}
+
+##
+# Provides a generic yes/no gui prompt. Returns 0 if yes and 1 if no
+function prompt_yn {
+    zenity --question --text="$@"
+    return $?
+}
+
+##
+# Make a temporary file
+function new_tempfile {
+    local NAME="$1"
+    local EXT="$2"
+    OUTPUT=$(mktemp "/tmp/$NAME-XXXXX") || die "Failed to create tmp file"
+}
+
+##
+# exit with the given message
+function die {
+    echo "$@" 1>&2
+    exit 2
+}
+
+main "$@"
+
